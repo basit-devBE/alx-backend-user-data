d75290216df553692721e646a342e817945e5891
basit <basitmohammed362@gmail.com>
2024-07-11 16:02:44 +0000

    hellllo

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,25 @@
 
 from api.v1.auth.auth import Auth
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Authentication class
     """
-    pass
+    user_id_by_session_id: dict = {}
+
+    def __init__(self) -> None:
+        """ SessionAuth constructor
+        """
+        pass
+        """ Session ID to User ID mapping """
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session ID for a user ID
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
