7013b77c6bba845d7289f4dbc43c63a03deb21b5
basit <basitmohammed362@gmail.com>
2024-07-16 15:20:57 +0000

    hello

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""hash user password
+"""
+
+import bcrypt
+from typing import List, Tuple
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
